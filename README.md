60c2d513ffe77849618e8e6fe47cdd110fcc04da
Daniel Lyne <DLyne@pm.me>
2021-05-16 04:02:50 -0400

    Basic functionality

---
+++ README.md
@@ -0,0 +1,2 @@
+# Tokio TLS
+A simple library to abstract interfacing Tokio TCP streams with Rustls sessions
---
+++ src/lib.rs
@@ -0,0 +1,133 @@
+use rustls::{ClientConfig, ClientSession, ServerConfig, ServerSession, Session};
+use std::{
+    io::{Read, Write},
+    net::SocketAddr,
+    rc::Rc,
+    sync::Arc,
+};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpStream,
+};
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
+
+pub enum Error {
+    SerializationError,
+    DeserializationError,
+    TlsError,
+    TcpError,
+}
+
+pub struct TlsConnection<T> {
+    session: T,
+    tcp_stream: TcpStream,
+}
+
+impl<T: Session> TlsConnection<T> {
+    fn new(session: T, tcp_stream: TcpStream) -> TlsConnection<T> {
+        TlsConnection {
+            session,
+            tcp_stream,
+        }
+    }
+
+    fn session(&self) -> &T {
+        &self.session
+    }
+
+    async fn send_message<U: serde::Serialize + std::fmt::Debug>(
+        &mut self,
+        msg: &U,
+    ) -> Result<(), Error> {
+        let serialized_msg = match bincode::serialize(msg) {
+            Ok(msg) => msg,
+            Err(e) => {
+                tracing::error!("Error serialzing message {:?}: {:?}", msg, e);
+                return Err(Error::SerializationError);
+            }
+        };
+
+        let mut buffer = Vec::new();
+
+        if let Err(e) = self.session.write(&serialized_msg) {
+            tracing::error!("Error writing message {:?} to ServerSession: {:?}", msg, e);
+            return Err(Error::TlsError);
+        };
+
+        if let Err(e) = self.session.write_tls(&mut buffer) {
+            tracing::error!(
+                "Error encoding message {:?} with ServerSession: {:?}",
+                msg,
+                e
+            );
+            return Err(Error::TlsError);
+        }
+
+        if let Err(e) = self.tcp_stream.write(&buffer).await {
+            tracing::error!(
+                "Error sending encoded message {:?} through TCP stream: {:?}",
+                msg,
+                e
+            );
+            return Err(Error::TcpError);
+        };
+
+        Ok(())
+    }
+
+    async fn read_message<U: serde::de::DeserializeOwned + std::fmt::Debug + Clone>(
+        &mut self,
+        msg: &[u8],
+    ) -> Result<Option<U>, Error> {
+        let mut buffer = Vec::new();
+
+        if let Err(e) = self.tcp_stream.read_buf(&mut buffer).await {
+            tracing::error!(
+                "Error receiving encoded messsage through TCP stream: {:?}",
+                e
+            );
+            return Err(Error::TcpError);
+        }
+
+        if let Err(e) = self.session.read_tls(&mut buffer.as_slice()) {
+            tracing::error!("Error reading message with ServerSession: {:?}", e);
+            return Err(Error::TlsError);
+        }
+
+        if let Err(e) = self.session.process_new_packets() {
+            tracing::error!("Error decoding message with ServerSession: {:?}", e);
+            return Err(Error::TlsError);
+        }
+
+        if self.session.is_handshaking() {
+            return Ok(None);
+        }
+
+        let mut buffer = Vec::new();
+
+        if let Err(e) = self.session.read_to_end(&mut buffer) {
+            tracing::error!(
+                "Error reading message from ServerSession to buffer: {:?}",
+                e
+            );
+            return Err(Error::TlsError);
+        }
+
+        let out = match bincode::deserialize::<U>(&mut buffer) {
+            Ok(deserialized) => deserialized,
+            Err(e) => {
+                tracing::error!("Error deserializing message: {:?}", e);
+                return Err(Error::DeserializationError);
+            }
+        };
+
+        Ok(Some(out))
+    }
+}
